97207eca896cd1b5e0bd5a241de7b4dd3ed5a66c
unknown <pedrohenriquefdasilva14@gmail.com>
2024-12-05 19:17:56 -0300

    feat: create TaskModel entity

---
+++ src/domain/models/TaskModel.js
@@ -0,0 +1,37 @@
+import sequelize from "../../config/database";
+import { DataTypes } from "sequelize";
+
+// Definição do modelo Task
+const Task = sequelize.define("Task", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'userId'  
+    }
+  }
+}, {
+  tableName: 'tasks',
+  timestamps: false,
+});
+
+Task.associate = (models) => {
+  Task.belongsTo(models.User, { foreignKey: 'userId' });
+};
+
+export default Task;
